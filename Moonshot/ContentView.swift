67a95e81adbca86c0daf2ef7c2fc66ce634219cf
mlwelker <nutria.watts.0g@icloud.com>
2023-08-16 16:11:43 -0500

    LazyVGrid adaptive

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -3,9 +3,7 @@ import SwiftUI
 
 struct ContentView: View {
     let layout = [
-        GridItem(.fixed(80)),
-        GridItem(.fixed(80)),
-        GridItem(.fixed(80))
+        GridItem(.adaptive(minimum: 80)),
     ]
     
     var body: some View {
