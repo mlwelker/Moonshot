a045e9fe4c5f3fc2a55c4d69984a267281899088
mlwelker <nutria.watts.0g@icloud.com>
2023-08-16 16:35:25 -0500

    starter files + Bundle decode extension

---
+++ Moonshot/Bundle-Decodable.swift
@@ -0,0 +1,22 @@
+
+import Foundation
+
+extension Bundle {
+    func decode(_ file: String) -> [String: Astronaut] {
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("failed to locate \(file) in bundle")
+        }
+        
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("failed to load \(file) from bundle")
+        }
+        
+        let decoder = JSONDecoder()
+        
+        guard let loaded = try? decoder.decode([String: Astronaut].self, from: data) else {
+            fatalError("failed to decode \(file) from bundle")
+        }
+        
+        return loaded
+    }
+}
